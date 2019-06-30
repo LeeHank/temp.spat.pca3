929f2a4ef386956d0bc52b452c40d93cc9f55ceb
LeeHank <ckshoupon@hotmail.com>
2019-06-30 23:27:04 +0800

    update data description

--- R/document_data.R
+++ R/document_data.R
@@ -6,3 +6,36 @@
 #' @format a 480(time)*289(location) data matrix
 "NCEP_U"
 
+#' @title  Coordinate matrix of wind speed dataset
+#' 
+#' @description Coordinate matrix of wind speed dataset
+#' @format a 289(location)*2(coordinates) data matrix
+"NCEPlatlon"
+
+#' @title  Simulation data
+#' 
+#' @description generate 
+#' @format a list contains:
+#' \describe{
+#' \item{y_mat}{simulation data matrix}
+#' \item{xsi_mat}{random coefficient matrix}
+#' \item{epsilon_mat}{measurement error matrix}
+#' \item{location}{location matirx for fitting}
+#' \item{phi_mat}{true phi matrix}
+#' \item{location_new}{location matrix for prediction}
+#' \item{phi_mat_new}{basis functions evaluated at location_new}
+#' }
+"sim_data"
+
+#' @title  Tuning results for sim_data
+#' 
+#' @description Tuning results for sim_data
+#' @format a list contains:
+#' \describe{
+#' \item{k_best}{the best K}
+#' \item{tau_best}{the best tau}
+#' \item{tau_list}{a list contains tuning process of tau in each K}
+#' \item{k_process}{the negativet two log-likelihood in each K}
+#' \item{best_list}{the detail of best tuning results}
+#' }
+"cv_result_all"
